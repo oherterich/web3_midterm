3ddc10fdb48d4e6b9174132e825af479713d32f6
Gustavo Faria <gusfaria11@gmail.com>
2013-10-09 15:32:33 -0400

    update layout

--- code/js/main.js
+++ code/js/main.js
@@ -1,6 +1,31 @@
-$document.ready(function(){
+
+
 	var key = '0bd1d2d41d65047dadb0ce85c999d9cd';
+	
 	
 
 
-)};
+
+
+function request(url, callback){
+  var xhr = new XMLHttpRequest();xhr.onreadystatechange = (function(myxhr){
+    return function() {
+      callback(myxhr);
+    }
+  })(xhr);
+  xhr.open('GET', url, true);
+  xhr.send('');
+}
+
+// -----------------------
+
+
+request("http://api.brewerydb.com/v2/?key=key",function(result) {
+  
+	console.log(result);
+  var rawJson = result.response;
+  var data = JSON.parse(rawJson);
+});
+
+
+

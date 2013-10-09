4904dd90239eb999663bb138540b33630d9a0dba
Gustavo Faria <gusfaria11@gmail.com>
2013-10-09 19:21:04 -0400

    update layout

--- code/js/main.js
+++ code/js/main.js
@@ -1,31 +1,37 @@
 
 
 	var key = '0bd1d2d41d65047dadb0ce85c999d9cd';
+	var url = 'http://api.brewerydb.com/v2/?key='+key;
 	
 	
+	function request(url, callback){
+		return function(){
+			callback(JSON);
+		}
+	}
 
 
 
 
-function request(url, callback){
-  var xhr = new XMLHttpRequest();xhr.onreadystatechange = (function(myxhr){
-    return function() {
-      callback(myxhr);
-    }
-  })(xhr);
-  xhr.open('GET', url, true);
-  xhr.send('');
-}
+// function request(url, callback){
+//   var xhr = new XMLHttpRequest();xhr.onreadystatechange = (function(myxhr){
+//     return function() {
+//       callback(myxhr);
+//     }
+//   })(xhr);
+//   xhr.open('GET', url, true);
+//   xhr.send('');
+// }
 
 // -----------------------
 
 
-request("http://api.brewerydb.com/v2/?key=key",function(result) {
+// request("http://api.brewerydb.com/v2/?key=key",function(result) {
   
-	console.log(result);
-  var rawJson = result.response;
-  var data = JSON.parse(rawJson);
-});
+// 	console.log(result);
+//   var rawJson = result.response;
+//   var data = JSON.parse(rawJson);
+// });
 
 
 

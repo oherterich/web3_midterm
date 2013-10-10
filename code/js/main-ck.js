4dfcf861a6db200085f972342f5e5389b3179215
Owen Herterich <oherterich@gmail.com>
2013-10-09 20:19:02 -0400

    added style and js

---
+++ code/js/main-ck.js
@@ -0,0 +1,6 @@
+/*$document.ready(function(){
+	var key = '0bd1d2d41d65047dadb0ce85c999d9cd';
+	
+
+
+)};*/var overlay=document.getElementById("overlay"),grid=document.getElementsByClassName("grid");for(var i=0;i<grid.length;i++){var s=grid.item(i);s.addEventListener("click",function(e){overlay.classList.remove("is-hidden")})}overlay.addEventListener("click",function(e){overlay.classList.add("is-hidden")});
--- code/js/main.js
+++ code/js/main.js
@@ -1,6 +1,21 @@
-$document.ready(function(){
+/*$document.ready(function(){
 	var key = '0bd1d2d41d65047dadb0ce85c999d9cd';
 	
 
 
-)};
+)};*/
+
+var overlay = document.getElementById('overlay');
+
+var grid = document.getElementsByClassName('grid');
+
+for (var i = 0; i < grid.length; i++) {
+	var s = grid.item(i);
+	s.addEventListener('click', function(evt) {
+		overlay.classList.remove('is-hidden');
+	});
+}
+
+overlay.addEventListener('click', function(evt) {
+	overlay.classList.add('is-hidden');
+});

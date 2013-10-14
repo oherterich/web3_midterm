03fac7c52160d64ae25828bd2fe6070096c9f7b1
Owen Herterich <oherterich@gmail.com>
2013-10-13 20:38:51 -0400

    added hover functionality

---
+++ code/js/hover.js
@@ -0,0 +1,20 @@
+var grid = document.getElementsByClassName('grid');
+for (var i = 0; i < grid.length; i++) {
+	var s = grid.item(i);
+	s.addEventListener('mouseover', function(evt) {
+		// var childNode = s.firstChild.grid_hover;
+		// var test = document.getElementsByClassName('grid_hover');
+		// 	test.firstChild.style.background="lime";
+
+		 //var gHover = this.getElementsByClassName('grid-hover');
+		 var gHover = this.querySelector('.grid-hover');
+		 gHover.style.paddingTop = 16.666666+'%';
+		 gHover.style.marginTop = -16.666666 + '%';
+	});
+	s.addEventListener('mouseout', function(evt) {
+		 var gHover = this.querySelector('.grid-hover');
+			gHover.style.paddingBottom = 0.0+'%';
+			gHover.style.paddingTop = 0.0+'%';
+			gHover.style.marginTop = 16.666666+'%';
+	});
+}
